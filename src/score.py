51a3b345aa3689c8d5d10995cefaff8c8cdd61c5
Florian Pydde <flpydde@microsoft.com>
2020-09-28 18:04:49 +0200

    Added model arg name to scoring

--- src/score.py
+++ src/score.py
@@ -1,7 +1,9 @@
 import joblib
 import pandas as pd
+import argparse 
 from azureml.core.model import Model
 from utils import retrieve_workspace
+
 # from inference_schema.schema_decorators import input_schema, output_schema
 # from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
 
@@ -10,18 +12,22 @@ model = None
 def init():
     global model
 
+    parser = argparse.ArgumentParser(description="Variables ")
+    parser.add_argument('--model_name', dest="model_name", default='mymodel.pkl', required=True)
+    args, _ = parser.parse_known_args()
+
     # If you deploy the model in a custom docker
     # ws = retrieve_workspace()
-    # model_reg = Model(ws, name = 'mymodel.pkl')
+    # model_reg = Model(ws, name = args.model_name)
     # model_reg.download('./model', exist_ok = False)
-    # model = joblib.load('./model/mymodel.pkl')
+    # model = joblib.load('./model/args.model_name)
 
     # Retrieve the path to the model file using the model name
-    model_path = Model.get_model_path('name of model')
+    model_path = Model.get_model_path(args.model_name)
     model = joblib.load(model_path)
 
 
-#if you want to show examples in swaager
+# if you want to show examples in swaager
 # input_sample = np.array([[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]])
 # output_sample = np.array([3726.995])
 
